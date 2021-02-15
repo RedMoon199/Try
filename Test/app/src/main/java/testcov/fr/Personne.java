bc6352ad292f84d172ee3c49359004bd6b9b84ab
Ananth <xignir@gmail.com>
2021-02-15 15:13:40 +0100

    Ajout attribut nbJour classe personne

--- Test/app/src/main/java/testcov/fr/Personne.java
+++ Test/app/src/main/java/testcov/fr/Personne.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 public class Personne {
 	private boolean contaminer;
 	private Color infected;
+	private int nbJour;
+
+	private final int DEFAULT_NBJOUR = 0;
 
 	public Personne(Boolean contaminer, Color infected){
 		this.contaminer = contaminer;
 		this.infected = infected;
+		this.nbJour = DEFAULT_NBJOUR;
 	}
 
 	public boolean getContaminer(){
@@ -25,6 +29,14 @@ public class Personne {
 		this.infected = color;
 	}
 
+	public int getNbJour(){
+		return this.nbJour;
+	}
+
+	public void setNbJour(int x)
+	{
+		this.nbJour = x;
+	} 
 	public void beInfected(){
 		setContaminer(true);
 		setInfected(Color.RED); //a voir par rapport a la couleur de l'infection 

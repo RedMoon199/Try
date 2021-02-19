f68736f0230f678d7fcd110bc1c08af0478c1c33
Florian Dayde <florian.dayde@laposte.net>
2021-02-19 19:38:46 +0100

    Développement des méthodes pour gérer la contamination des gens dans les classes Personne et Environnement.

--- Test/app/src/main/java/testcov/fr/Environnement.java
+++ Test/app/src/main/java/testcov/fr/Environnement.java
@@ -25,14 +25,18 @@ public class Environnement {
         this.tab =  new Personne[this.TEST_NB_LIGNE][this.TEST_NB_COLONNE];
         this.cpy = new Personne[this.TEST_NB_LIGNE][this.TEST_NB_COLONNE];
 
+        //On créé un compteur pour initialiser les id des personnes
+        int countId = 1;
+
         for(int i = 0; i < this.TEST_NB_LIGNE; i++)
         {
             for(int j = 0; j < this.TEST_NB_COLONNE; j++)
             {
                 if((i % 2 == 1) && (j % 2 == 1))
                 {
-                    this.tab[i][j] = new Personne(false, this.varTMPdieRate);
-                    this.cpy[i][j] = new Personne(false, this.varTMPdieRate);
+                    this.tab[i][j] = new Personne(false, this.varTMPdieRate, countId);
+                    this.cpy[i][j] = new Personne(false, this.varTMPdieRate, countId);
+                    countId += 1;
                 }
                 else
                 {
@@ -83,6 +87,7 @@ public class Environnement {
         return count;
     }
 
+    // fct qui permet de déplacer une seule personne
     private void deplacement(int i, int j)
     {
         if(this.cpy[i][j] != null) // Si la case est occupée par une personne
@@ -150,6 +155,91 @@ public class Environnement {
                 {
                     this.cpy[i][j] = null;
                 }
+            }
+        }
+    }
+
+    // Renseigne la liste des personnes rencontrés par une personne malade.
+    private void updateListMeet(int i, int j)
+    {
+        if(this.tab[i][j] != null)
+        {
+            // Si la personne est malade, on va chercher les personnes saines autour d'elle et les ajouter à sa liste de rencontre.
+            if(this.tab[i][j].getSeek())
+            {
+                int x, toX, y, toY;
+                if(i > 0)
+                    x = i - 1;
+                else
+                    x = i;
+                if(i < (this.TEST_NB_LIGNE - 1))
+                    toX = i + 1;
+                else
+                    toX = i;
+                if(j > 0)
+                    y = j - 1;
+                else
+                    y = j;
+                if(j < (this.TEST_NB_COLONNE - 1))
+                    toY = j + 1;
+                else
+                    toY = j;
+
+                for(int k = x; k <= toX; k++)
+                {
+                    for(int l = y; l <= toY; l++)
+                    {
+                        if((k != i) && (l != j))
+                        {
+                            this.tab[i][j].haveMeet(this.tab[k][l]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Recherche une personne par son identifiant et l'infecte
+    private void searchAndInfect(int id)
+    {
+        for(int i = 0; i < this.TEST_NB_LIGNE; i++)
+        {
+            for(int j = 0; j < this.TEST_NB_COLONNE; j++)
+            {
+                if(this.tab[i][j] != null)
+                {
+                    if(this.tab[i][j].getId() == id)
+                        this.tab[i][j].setSeek(true);
+                }
+            }
+        }
+    }
+
+    // La personne ciblée infecte les personnes de sa liste selon le R0 puis remet sa liste à 0
+    // De plus elle met à jour sa maladie
+    private void endTurnInfectPeople(int x, int y)
+    {
+        if(this.tab[x][y] != null)
+        {
+            if(this.tab[x][y].getSeek() && (!this.tab[x][y].getIsDead()))
+            {
+                int R0 = this.form.getR0();
+                int size = this.tab[x][y].getNbMeet();
+                int newInfection, idNewInfected;
+                if(R0 > size)
+                    newInfection = size;
+                else
+                    newInfection = R0;
+
+                while(newInfection != 0)
+                {
+                    idNewInfected = this.tab[x][y].getIdRencontre(newInfection);
+                    searchAndInfect(idNewInfected);
+                    newInfection -= 1;
+                }
+
+                this.tab[x][y].resetMeet();
+                this.tab[x][y].updateSeek();
             }
         }
     }
--- Test/app/src/main/java/testcov/fr/Personne.java
+++ Test/app/src/main/java/testcov/fr/Personne.java
@@ -1,5 +1,7 @@
 package testcov.fr;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Personne {
@@ -11,6 +13,8 @@ public class Personne {
     private int dieRate;
     //private boolean hasBeenSeek;
     private Random rdm = new Random();
+    private int id;
+    private List<Integer> rencontre;
 
     private static final boolean DEFAULT_DEATH = false;
     private static final int DEFAULT_SEEK_ADVANCEMENT = 0;
@@ -18,12 +22,14 @@ public class Personne {
     // Nombre de jour au bout duquel un malade n'est plus malade.
     private static final int DEFAULT_STOP_SEEK = 15;
 
-    public Personne( boolean seek, int dieRate )
+    public Personne( boolean seek, int dieRate, int id )
     {
         this.seek = seek;
         this.isDead = this.DEFAULT_DEATH;
         this.seekAdvancement = this.DEFAULT_SEEK_ADVANCEMENT;
         this.dieRate = dieRate;
+        this.id = id;
+        rencontre = new ArrayList<Integer>();
     }
 
     //constructeur par copie (est-ce qu'on en a besoin ?)
@@ -33,6 +39,12 @@ public class Personne {
         this.isDead = p.isDead;
         this.seekAdvancement = p.seekAdvancement;
         this.dieRate = p.dieRate;
+        this.id = p.id;
+        int size = p.rencontre.size();
+        for(int i = 0; i < size; i++)
+        {
+            this.rencontre.add(p.rencontre.get(i); // Est-ce que les indices commencent bien à 0 ?
+        }
     }*/
 
     // Met à jour la maladie de chacun
@@ -61,6 +73,36 @@ public class Personne {
         }
     }
 
+    // Permet d'ajouter une personne à la liste des personnes rencontrées, si elle n'est pas déjà dans la liste et si elle n'est pas malade
+    public void haveMeet(Personne p)
+    {
+        // Si la personne n'est pas malade et pas morte
+        if((!p.getSeek()) && (!p.getIsDead()))
+        {
+            int id = p.getId();
+            int size = this.rencontre.size();
+            boolean dejaVu = false;
+            for(int i = 0; i < size; i++)
+            {
+                if(this.rencontre.get(i) == id)
+                {
+                    dejaVu = true;
+                }
+            }
+
+            // Si pas déjà Vu, on ajoute à la liste.
+            if(!dejaVu)
+            {
+                this.rencontre.add(id);
+            }
+        }
+    }
+
+    public void resetMeet()
+    {
+        this.rencontre.clear();
+    }
+
     public boolean getSeek()
     {
         return this.seek;
@@ -75,6 +117,22 @@ public class Personne {
     {
         return this.isDead;
     }
+
+    public int getId() { return this.id; }
+
+    // Permet de connaître le nombre de rencontre de la personne dans la journée
+    public int getNbMeet()
+    {
+        return this.rencontre.size();
+    }
+
+    // Permet de connaître l'id de la personne à l'incice tant dans la liste des personnes rencontrées
+    public int getIdRencontre(int indice)
+    {
+        int realIndice = indice - 1;
+        return this.rencontre.get(realIndice);
+    }
+
 
     public void setSeek( boolean seek )
     {

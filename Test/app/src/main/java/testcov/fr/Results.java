0cb4c76baa6833649392e7e309bc7213e69d2c73
Ananth <xignir@gmail.com>
2021-02-28 16:56:06 +0100

    Ajout result.class + modif activity_result

--- Test/app/src/main/java/testcov/fr/Results.java
+++ Test/app/src/main/java/testcov/fr/Results.java
@@ -2,6 +2,7 @@ package testcov.fr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,23 +17,35 @@ public class Results extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout./*activité_correpsondant au développement du virus*/);
+        setContentView(R.layout.activity_propagation/*activité_correpsondant au développement du virus*/);
+        il faut modifié l'activbité qui va envoyé les infos '
 
         Intent intent = getIntent();
         if(intent != null)
         {
             if(intent.hasExtra("nbPersonnes")){
                 nbPersonnes = intent.getIntExtra("nbPersonnes",0 );
+                TextView textNbPersonnes = (TextView) findViewById(R.id.nbPersonnes);
+                textNbPersonnes.setText("Sur " + nbPersonnes + " Il y a eu :");
+
             }
             if(intent.hasExtra("nbMort")){
                 nbMort = intent.getIntExtra("nbMort",0 );
+                TextView textNbMort = (TextView) findViewById(R.id.nbMortRes);
+                textNbMort.setText("- " + nbMort + "Mort");
             }
             if(intent.hasExtra("nbContamine")){
                 nbContamine = intent.getIntExtra("nbContamine",0 );
+                TextView textNbContamine = (TextView) findViewById(R.id.nbContamine);
+                textNbContamine.setText("- " + nbContamine + " personnes Contiminé(s)");
             }
             if(intent.hasExtra("nbSain")){
                 nbSain = intent.getIntExtra("nbSain",0 );
+                TextView textNbSain = (TextView) findViewById(R.id.nbSain);
+                textNbSain.setText("- " + nbSain+ " personne(s) encore saine(s)");
             }
         }
     }
+
+
 }
